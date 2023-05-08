2b85cced736991c25404601d2fdf60335d4ba071
Ashish Sahu <ashishsahu10428@gmail.com>
2023-05-09 01:32:20 +0530

    Navbar Final with colour theme

--- popcorn/src/app/components/Header.js
+++ popcorn/src/app/components/Header.js
@@ -1,32 +1,60 @@
+"use client";
 import React from "react";
 import styles from "../styles/navbar.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Header() {
+  const [showNav, setShowNav] = React.useState(false);
+
+  const toggleNavItems = () => {
+    console.log(showNav);
+    setShowNav(!showNav);
+  };
+
+  const getstyle = () => {
+    if (showNav) {
+      return styles.active + " " + styles.nav;
+    } else {
+      return styles.nav;
+    }
+  };
+
   return (
     <header className={styles.navbar}>
-      <div className={styles.logo}>
-        <Image
-          src="/logo.png"
-          alt="logo Image"
-          width="200"
-          height="70"
-          unoptimized={true}
-        />
-      </div>
-      <div className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/movies">Movies</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-        </ul>
+      <div className={styles.container}>
+        <div className={styles.logo}>
+          {/* <Image
+            src="/logo.png"
+            alt="logo Image"
+            width="100"
+            height="35"
+            unoptimized={true}
+          /> */}
+          <h1>popcorn</h1>
+        </div>
+        <div className={styles.menuicon} onClick={toggleNavItems}>
+          EE
+        </div>
+        <div className={getstyle()}>
+          <ul>
+            <li>
+              <Link href="/" className={styles.lnk}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/movies" className={styles.lnk}>
+                Movies
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" className={styles.lnk}>
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
