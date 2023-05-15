9f474a0e23d767b078c762c6a6f1e4e2ac0297db
Ashish Sahu <ashishsahu10428@gmail.com>
2023-05-16 02:01:09 +0530

    Movie page with dynamic ID added ✅

--- popcorn/next.config.js
+++ popcorn/next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ["occ-0-3933-116.1.nflxso.net"],
+    domains: ["occ-0-3933-116.1.nflxso.net", "occ.a.nflxso.net"],
+    allowFutureImage: true,
   },
 };
 
--- popcorn/src/app/movies/[id]/page.js
+++ popcorn/src/app/movies/[id]/page.js
@@ -1,4 +1,6 @@
 import React from "react";
+import styles from "../../styles/moviespage.module.css";
+import Image from "next/image";
 
 export default async function page({ params }) {
   const id = params.id;
@@ -19,8 +21,56 @@ export default async function page({ params }) {
   }
 
   const main_data = data[0].details;
-  return <>
-    <h1>{main_data.title}</h1>
-    <h3>{main_data.synopsis}</h3>
-  </>;
+  const genres = main_data.genres;
+
+
+  return (
+    <>
+      <div className={styles.main}>
+        <div
+          className={styles.banner}
+          style={{
+            backgroundImage:
+              "linear-gradient(rgba(255, 0, 141, 0.7), rgba(255, 0, 141, 0.7))," +
+              `url('${main_data.backgroundImage.url}')`,
+          }}
+        >
+          <h1>{main_data.title}</h1>
+        </div>
+
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <p>
+              Released on - <span>{main_data.releaseYear}</span>{" "}
+            </p>
+            <div className={styles.tags}>
+              <ul>
+                {genres.map((item, index) => {
+                  return <li key={index}>{item.name}</li>;
+                })}
+              </ul>
+            </div>
+            <div className={styles.cast}>
+              <ul>
+                {main_data.cast.map((item, index) => {
+                  return <li key={index}>{item.name}</li>;
+                })}
+              </ul>
+            </div>
+          </div>
+          <div className={styles.right}>
+            <div className={styles.nameImg}>
+              <Image src={main_data.logoImage.url} alt="poster" fill />
+            </div>
+            <div className={styles.bannerImg}>
+              <Image src={main_data.backgroundImage.url} alt="poster" fill />
+            </div>
+            <div className={styles.abt}>
+              <p>{main_data.synopsis}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
